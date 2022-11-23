c896d0abe839c2641aa06726fcbacbcf7b8b6a30
Prachi Bala <prachibala330@gmail.com>
2022-11-24 02:18:07 +0600

    ver-0.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Router";
 import "./App.css";
 
 function App() {
-    return <div className=""></div>;
+    return (
+        <div className="">
+            <RouterProvider router={router} />
+        </div>
+    );
 }
 
 export default App;
